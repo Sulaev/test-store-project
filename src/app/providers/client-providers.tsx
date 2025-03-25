204483fae29312f4fd5d256957026b9c41001339
Sulaev <sulaev2004@mail.ru>
2025-03-25 16:28:57 +0300

    fix client-providers.tsx & add layout.module.css

--- src/app/providers/client-providers.tsx
+++ src/app/providers/client-providers.tsx
@@ -2,8 +2,18 @@
 
 import { type ReactNode } from "react";
 import { ThemeProvider } from "./theme-provider";
+import { useEffect, useState } from "react";
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
   return (
     <ThemeProvider
       attribute="class"
